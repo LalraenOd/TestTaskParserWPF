f89c1ff0adada8026574eb23d8c0aea771f6471e
Владимир Миненко <minenko.od@gmail.com>
2021-05-25 15:40:06 +0300

    model data writing to db success

---
+++ TestTaskParserWPF/Core/ModelData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestTaskParserWPF.Core
+{
+    internal class ModelData
+    {
+        internal ModelData(string modelId, string modelName, string modelDateRange, string modelPickingCode)
+        {
+            ModelId = modelId ?? throw new ArgumentNullException(nameof(modelId));
+            ModelName = modelName ?? throw new ArgumentNullException(nameof(modelName));
+            ModelDateRange = modelDateRange ?? throw new ArgumentNullException(nameof(modelDateRange));
+            ModelPickingCode = modelPickingCode ?? throw new ArgumentNullException(nameof(modelPickingCode));
+        }
+
+        internal string ModelId { get; set; }
+
+        internal string ModelName { get; set; }
+
+        internal string ModelDateRange { get; set; }
+
+        internal string ModelPickingCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"Model name: {ModelName};\nModel Id: {ModelId};\n" +
+                $"Model Date Range: {ModelDateRange};\nModel Picking code: {ModelPickingCode}";
+        }
+    }
+}
--- TestTaskParserWPF/Core/WebPageWorker.cs
+++ TestTaskParserWPF/Core/WebPageWorker.cs
@@ -6,7 +6,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using TestTaskParserWPF.Core;
 
 namespace TestTaskParserWPF
 {
@@ -19,8 +19,8 @@ namespace TestTaskParserWPF
         /// <param name="url">URL to parse</param>
         internal static void WebPageWorker(string DBConnectionString, string url)
         {
-            //ParseModels(url);
-            PickingParser(url);
+            ParseModels(url);
+            //PickingParser(url);
         }
 
         /// <summary>
@@ -40,9 +40,9 @@ namespace TestTaskParserWPF
         }
 
         /// <summary>
-        /// Parsing
+        /// Parsing model names
         /// </summary>
-        /// <param name="webPageHtml">html source</param>
+        /// <param name="url">Page link to parse model names</param>
         private static void ParseModels(string url)
         {
             var webPageHtml = GetWebPage(url);
@@ -54,39 +54,47 @@ namespace TestTaskParserWPF
             foreach (var model in models)
             {
                 string modelName = model.TextContent;
-                Logger.LogMsg($"FOUND MODEL: {modelName}");
                 //Going back to model <div List Class>
                 IElement modelParent = model.ParentElement.ParentElement;
                 //Cheking number of model codes
                 int childrenCount = modelParent.QuerySelector("div.List").ChildElementCount;
-                Logger.LogMsg($"CHILDREN FOUND: {childrenCount}");
                 //Parsing child elements (model codes)
+                Logger.LogMsg($"Found {childrenCount} models on page.");
                 IElement[] childrenElements = modelParent.QuerySelectorAll("div.List").Children("div.List").ToArray();
-                for (int i = 0; i < childrenCount; i++)
+                for (int counter = 0; counter < childrenCount; counter++)
                 {
-                    string modelId = childrenElements[i].QuerySelector("div.List > div.List > div.id").TextContent;
-                    string modelIdHref = childrenElements[i].QuerySelector("div.List > div.List > div.id > a").GetAttribute("href");
-                    string modelDateRange = childrenElements[i].QuerySelector("div.List > div.List > div.dateRange").TextContent;
-                    string modelPicking = childrenElements[i].QuerySelector("div.List > div.List > div.modelCode").TextContent;
-                    Logger.LogMsg($"{modelName} {modelId} {modelDateRange} {modelPicking}");
-                    PickingParser("https://www.ilcats.ru" + modelIdHref);
+                    Logger.LogMsg($"Parsing {counter} model");
+                    //Smth to do with models data
+                    string modelId = childrenElements[counter].QuerySelector("div.List > div.List > div.id").TextContent;
+                    string modelIdHref = childrenElements[counter].QuerySelector("div.List > div.List > div.id > a").GetAttribute("href");
+                    string modelDateRange = childrenElements[counter].QuerySelector("div.List > div.List > div.dateRange").TextContent;
+                    string modelPickingCode = childrenElements[counter].QuerySelector("div.List > div.List > div.modelCode").TextContent;
+                    ModelData modelData = new ModelData(modelId, modelName, modelDateRange, modelPickingCode);
+                    Logger.LogMsg($"Writing {counter} model to db...");
+                    DBWriterModelData(modelData);
+                    //PickingParser("https://www.ilcats.ru" + modelIdHref);
                 }
             }
         }
 
+        /// <summary>
+        /// Parsing each model pickings table
+        /// </summary>
+        /// <param name="url">Link to pickings webpage</param>
         private static void PickingParser(string url)
-        {                    
+        {
             //Parsing car pickings (tables)
             Logger.LogMsg($"Parsing car pickings by link: {url}");
-            string pickingWebPage = GetWebPage(url);            
+            string pickingWebPage = GetWebPage(url);
             HtmlParser parser = new HtmlParser();
             IHtmlDocument htmlDocument = parser.ParseDocument(pickingWebPage);
-            IElement firstTable = htmlDocument.QuerySelector("tbody");            
+            IElement firstTable = htmlDocument.QuerySelector("tbody");
             IHtmlCollection<IElement> pickingTable = firstTable.QuerySelectorAll("tbody > tr");
             IElement[] pickingTableHeaders = pickingTable[0].QuerySelectorAll("th").ToArray();
             MainWindow.AppWindow.RichTextBoxLog.AppendText("\n");
             foreach (var header in pickingTableHeaders)
             {
+                //Smth to do with headers
                 MainWindow.AppWindow.RichTextBoxLog.AppendText(header.TextContent + "\n");
             }
             for (int tableRow = 1; tableRow < pickingTable.Length; tableRow++)
@@ -95,18 +103,40 @@ namespace TestTaskParserWPF
                 IElement[] cellElements = pickingTable[tableRow].QuerySelectorAll("td").ToArray();
                 foreach (var cellElement in cellElements)
                 {
+                    //Smth to do with each table cell
                     MainWindow.AppWindow.RichTextBoxLog.AppendText(cellElement.TextContent + "\n");
                 }
             }
         }
 
-        private static void DBWriter(string DBConnectionString)
+        private static void DBWriterModelData(ModelData modelData)
         {
+            string DBConnectionString = MainWindow.AppWindow.TextBoxSQLConnectionString.Text;
             using (SqlConnection sqlConnection = new SqlConnection(DBConnectionString))
             {
                 sqlConnection.Open();
+                try
+                {
+                    string sqlExpresion = $"INSERT INTO ModelData (modelId, modelName, modelDateRange, modelPickingCode) " +
+                        $"VALUES ('{modelData.ModelId}','{modelData.ModelName}','{modelData.ModelDateRange}','{modelData.ModelPickingCode}')";
+                    Logger.LogMsg($"\nsqlexp :{sqlExpresion}\n");
+                    SqlCommand command = new SqlCommand(sqlExpresion, sqlConnection);
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogMsg(ex.ToString());
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
-            throw new NotImplementedException();
+        }
+
+        private static void DbWriterPickingData()
+        {
+
         }
     }
 }
